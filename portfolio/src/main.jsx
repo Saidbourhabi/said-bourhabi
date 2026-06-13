b6e1c1db09384e14dff9902edfd4f221715c3cc2
Said-bourhabi <abousaid11araqi@gmail.com>
2026-06-13 17:24:51 +0100

    feat: implement contact form with EmailJS, and Vite env keys

--- portfolio/src/main.jsx
+++ portfolio/src/main.jsx
@@ -4,6 +4,8 @@
   import { HelmetProvider } from 'react-helmet-async'
   import './index.css'
   import App from './App.jsx'
+  import emailjs from '@emailjs/browser';
+  emailjs.init(import.meta.env.VITE_EMAILJS_PUBLIC_KEY);
 
   createRoot(document.getElementById('root')).render(
     <StrictMode>
